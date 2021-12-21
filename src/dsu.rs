906e6dc7201bc578e2741661468341429424ee2a
William Kraft (elev) <william.kraft@elevmail.stockholm.se>
2021-12-21 19:35:09 +0100

    Added the ability to have data where max will be at top n

--- src/dsu.rs
+++ src/dsu.rs
@@ -1,50 +1,52 @@
-pub struct DSU {
+pub struct DSU <T> where T : std::cmp::PartialOrd{
     parents : Vec<usize>,
+    data : Vec<T>
 }
 
-impl DSU {
-    pub fn default() -> DSU {
-        DSU{parents : Vec::new()}
+impl <T> DSU<T> where T : std::cmp::PartialOrd {
+    pub fn new() -> DSU<T> {
+        DSU{parents : Vec::new(), data : Vec::new()}
     }
 
-    pub fn new(count : usize) -> DSU{
-        let mut dsu = DSU::with_capacity(count);
-        dsu.append(count);
-        dsu
-    }
-
-    pub fn with_capacity(capacity : usize) -> DSU {
-        DSU{parents : Vec::with_capacity(capacity)}
+    pub fn with_capacity(capacity : usize) -> DSU<T> {
+        DSU{parents : Vec::with_capacity(capacity), data : Vec::with_capacity(capacity)}
     }
     
-    pub fn from(parents : Vec<usize>) -> DSU {
-        DSU{parents : parents}
+    pub fn from(parents : Vec<usize>, data : Vec<T>) -> DSU<T> {
+        DSU{parents : parents, data : data}
     }
 
     pub fn len(&self) -> usize {
         self.parents.len()
     }
 
-    pub fn push(&mut self) -> usize {
-        let len = self.parents.len();
+    pub fn push(&mut self, data : T) -> usize {
+        let len = self.len();
         self.parents.push(len);
+        self.data.push(data);
         len
     }
 
-    pub fn append(&mut self, count : usize) {
-        for _i in 0..count {
-            self.push();
-        }
-    }
-
     pub(crate) fn set_parent(&mut self, node : usize, parent : usize) {
-        debug_assert!(node < self.parents.len());
+        debug_assert!(node < self.len());
         self.parents[node] = parent
     }
 
     pub(crate) fn get_parent(&self, node : usize) -> usize {
-        debug_assert!(node < self.parents.len());
+        debug_assert!(node < self.len());
         self.parents[node]
+    }
+
+    pub(crate) fn set_data(&mut self, node : usize, data : T) {
+        self.data[node] = data;
+    }
+
+    pub fn get_data_mut(&mut self, node : usize) -> &mut T {
+        &mut self.data[node]
+    }
+
+    pub(crate) fn get_data(&self, node : usize) -> &T {
+        &self.data[node]
     }
 
     pub fn top(&self, node : usize) -> usize {
@@ -60,10 +62,27 @@ impl DSU {
         top
     }
 
-    pub fn union(&mut self, node0 : usize, node1 : usize) {
+    pub fn top_data(&self, node : usize) -> &T {
+        self.get_data(self.top(node))
+    }
+
+    pub fn top_data_mut(&mut self, node : usize) -> &mut T{
+        let top = self.top_mut(node);
+        self.get_data_mut(top)
+    }
+
+    pub fn union(&mut self, node0 : usize, node1 : usize) { 
         let top0 = self.top_mut(node0);
         let top1 = self.top_mut(node1);
 
-        self.set_parent(top1, top0);
+        let data0 = self.get_data(node0);
+        let data1 = self.get_data(node1);
+        
+        if data0 >= data1 {
+            self.set_parent(top1, top0);
+        }
+        else {
+            self.set_parent(top0, top1);
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,11 @@ mod tests {
 
     #[test]
     fn union() {
-        let mut  dsu = DSU::new(10);
+        let mut  dsu : DSU<i32> = DSU::new();
+
+        for i in 0..10 {
+            dsu.push(i);
+        }
 
         dsu.union(0, 1);
         dsu.union(1, 2);
@@ -19,6 +23,11 @@ mod tests {
 
         assert_eq!(dsu.top_mut(0), dsu.top_mut(3));
         assert_ne!(dsu.top_mut(1), dsu.top_mut(4));
+
+        assert_eq!(dsu.top_data(0), &3);
+        assert_eq!(dsu.top_data(1), &3);
+        assert_eq!(dsu.top_data(2), &3);
+        assert_eq!(dsu.top_data(3), &3);
     }
 
 }
